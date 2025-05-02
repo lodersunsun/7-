bd1894263aa922fc4a1a7fbadc683fa9381e3c74
lodersunsun <lodersun@gmail.com>
2025-05-03 01:56:11 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,20 @@
-const CACHE_NAME = "eggcalc-cache-v1";
-const OFFLINE_URLS = [
-  "/-/",
-  "/-/index.html",
-  "/-/manifest.json",
-  "/-/icon-192.png",
-  "/-/icon-512.png"
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(OFFLINE_URLS);
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('egg-report').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
     })
   );
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
